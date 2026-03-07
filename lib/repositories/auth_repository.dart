d5008d8d81da8e391976bb5ee3831a78b367088f
Roman <roman.baytala@gmail.com>
2026-03-07 12:07:38 +0200

    Add user model. Add abstract class for further implementation. Add validator for user input data.

---
+++ lib/repositories/auth_repository.dart
@@ -0,0 +1,8 @@
+import 'package:iot_flutter_lab/models/user.dart';
+
+abstract class IAuthRepository {
+  Future<bool> register(UserModel user);
+  Future<bool> login(String email, String password);
+  Future<UserModel?> getCurrentUser();
+  Future<void> logout();
+}
